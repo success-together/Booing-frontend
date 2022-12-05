e71ba7fc8ea5c10f4331ba0e9ea37150a43c2c56
oleksandr-harvart <oharvart7@gmail.com>
2022-12-05 11:15:19 +0100

    add upload image

--- src/shared/slices/Fragmentation/FragmentationService.ts
+++ src/shared/slices/Fragmentation/FragmentationService.ts
@@ -5,7 +5,7 @@ import {fetchWithTimeout} from '../../Executor';
 export const checkForDownloads = async (data: {user_id: string}) => {
   try {
     const url = BaseUrl + '/logged-in-user/checkForDownloads';
-    await fetchWithTimeout(url, data, 60000,true);
+    // await fetchWithTimeout(url, data, 60000,true);
   } catch (error) {
     console.log(error);
   }
@@ -14,7 +14,7 @@ export const checkForDownloads = async (data: {user_id: string}) => {
 export const checkForUploads = async (data: {user_id: string}) => {
   try {
     const url = BaseUrl + '/logged-in-user/checkForUploads';
-    await fetchWithTimeout(url, data, 10000,false);
+    // await fetchWithTimeout(url, data, 10000,false);
   } catch (error) {
     console.log(error);
   }
