414bf10aed94b8fd51d850370617155fd6387804
oleksandr-harvart <oharvart7@gmail.com>
2022-12-20 22:09:07 +0100

    fix base url

--- src/screens/DashboardContainer/Files/Uploads/LayoutWrapper/getUploadedDataHook.ts
+++ src/screens/DashboardContainer/Files/Uploads/LayoutWrapper/getUploadedDataHook.ts
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import {BaseUrl, store} from '../../../../../shared';
+import {setRootLoading} from '../../../../../shared/slices/rootSlice';
+import Toast from 'react-native-toast-message';
 
 const useGetUploadData = async (type?: string): Promise<any> => {
   const user_id = store.getState().authentication.userId;
 
   if (user_id && type !== '') {
     try {
+      store.dispatch(setRootLoading(true));
       const res = await axios({
         method: 'GET',
         url: `${BaseUrl}/logged-in-user/downloadFile/${user_id}/${type}`,
@@ -24,8 +27,14 @@ const useGetUploadData = async (type?: string): Promise<any> => {
           isImage: type === 'image',
         }));
       }
-    } catch (e) {
-      console.log({error: e});
+    } catch (e: any) {
+      Toast.show({
+        type: 'error',
+        text1: 'there was an error with fetching files',
+        text2: e.message,
+      });
+    } finally {
+      store.dispatch(setRootLoading(false));
     }
   }
 
