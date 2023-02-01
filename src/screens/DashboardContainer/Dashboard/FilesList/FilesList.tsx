c88b17c9a27971bc9ac1673b601c04faecb0d090
oleksandr-harvart <oharvart7@gmail.com>
2023-02-01 08:46:37 +0100

    remove loaded items from clearData

--- src/screens/DashboardContainer/Dashboard/FilesList/FilesList.tsx
+++ src/screens/DashboardContainer/Dashboard/FilesList/FilesList.tsx
@@ -123,31 +123,31 @@ export default function FilesList({
     disabled: false,
     show: false,
   });
-  const [items, setItems] = useState([]);
-  const [iterator, setIterator] = useState<Iterator<[]>>();
-  const [viewedItems, setViewedItems] = useState<
-    {id: string; isLoaded: boolean}[]
-  >([]);
-  const [loadedItemsIds, setLoadedItemsIds] = useState<string[]>([]);
+  // const [items, setItems] = useState([]);
+  // const [iterator, setIterator] = useState<Iterator<[]>>();
+  // const [viewedItems, setViewedItems] = useState<
+  //   {id: string; isLoaded: boolean}[]
+  // >([]);
+  // const [loadedItemsIds, setLoadedItemsIds] = useState<string[]>([]);
 
-  const onViewableItemsChanged = ({
-    viewableItems,
-  }: {
-    viewableItems: ViewToken[];
-  }) => {
-    setViewedItems(prev => {
-      const newViewedItems = [];
-      for (const viewToken of viewableItems) {
-        const isExist = prev.findIndex(e => e.id === viewToken.item.id) !== -1;
-        if (!isExist) {
-          newViewedItems.push({id: viewToken.item.id, isLoaded: false});
-        }
-      }
+  // const onViewableItemsChanged = ({
+  //   viewableItems,
+  // }: {
+  //   viewableItems: ViewToken[];
+  // }) => {
+  //   setViewedItems(prev => {
+  //     const newViewedItems = [];
+  //     for (const viewToken of viewableItems) {
+  //       const isExist = prev.findIndex(e => e.id === viewToken.item.id) !== -1;
+  //       if (!isExist) {
+  //         newViewedItems.push({id: viewToken.item.id, isLoaded: false});
+  //       }
+  //     }
 
-      return newViewedItems.length > 0 ? [...prev, ...newViewedItems] : prev;
-    });
-  };
-  const viewabilityConfigCallbackPairs = useRef([{onViewableItemsChanged}]);
+  //     return newViewedItems.length > 0 ? [...prev, ...newViewedItems] : prev;
+  //   });
+  // };
+  // const viewabilityConfigCallbackPairs = useRef([{onViewableItemsChanged}]);
 
   const onPress = useCallback(
     (id: string) => {
@@ -160,9 +160,9 @@ export default function FilesList({
   );
 
   const onDeleteFilesPress = useCallback(async () => {
-    setItems([]);
+    // setItems([]);
     const paths = selectedFilesIds.reduce<string[]>((acc, id) => {
-      const item = items.find((e: RenderFileData['item']) => e.id === id);
+      const item = data.find((e: RenderFileData['item']) => e.id === id);
       if (item) {
         acc.push((item as any).path);
         return acc;
@@ -213,7 +213,7 @@ export default function FilesList({
         text1: 'items deleted successfully',
       });
     }
-  }, [selectedFilesIds, items, refetchByLabel]);
+  }, [selectedFilesIds, refetchByLabel]);
 
   const onDeleteAppsPress = async () => {
     const apps = selectedFilesIds.reduce<any[]>((acc, id) => {
@@ -254,30 +254,31 @@ export default function FilesList({
           visibleCacheSize={visibleCacheSize}
           selected={selectedFilesIds.includes(id)}
           Icon={icons[label]}
-          loaded={() => {
-            setLoadedItemsIds(prev =>
-              prev.includes(id) ? prev : [...prev, id],
-            );
-          }}
+          loaded={() => void 0}
+          // loaded={() => {
+          //   setLoadedItemsIds(prev =>
+          //     prev.includes(id) ? prev : [...prev, id],
+          //   );
+          // }}
         />
       );
     },
-    [selectedFilesIds, onPress, data, items],
+    [selectedFilesIds, onPress, data],
   );
 
-  useEffect(() => {
-    setViewedItems(prev => {
-      let changed = false;
-      for (const item of prev) {
-        if (loadedItemsIds.includes(item.id) && !item.isLoaded) {
-          item.isLoaded = true;
-          changed = true;
-        }
-      }
+  // useEffect(() => {
+  //   setViewedItems(prev => {
+  //     let changed = false;
+  //     for (const item of prev) {
+  //       if (loadedItemsIds.includes(item.id) && !item.isLoaded) {
+  //         item.isLoaded = true;
+  //         changed = true;
+  //       }
+  //     }
 
-      return changed ? [...prev] : prev;
-    });
-  }, [viewedItems, loadedItemsIds]);
+  //     return changed ? [...prev] : prev;
+  //   });
+  // }, [viewedItems, loadedItemsIds]);
 
   useEffect(() => {
     if (selectedFilesIds.length !== 0 && !deleteBtnProps.show) {
@@ -294,61 +295,61 @@ export default function FilesList({
     };
   }, [selectedFilesIds]);
 
-  const nextSet = useCallback(
-    (initial?: Iterator<[]>) => {
-      console.log(items.length, data.length);
-      if (items.length === data.length) {
-        return;
-      }
+  // const nextSet = useCallback(
+  //   (initial?: Iterator<[]>) => {
+  //     console.log(items.length, data.length);
+  //     if (items.length === data.length) {
+  //       return;
+  //     }
 
-      if (viewedItems.filter(item => !item.isLoaded).length !== 0) {
-        return Toast.show({
-          type: 'info',
-          text1: label,
-          text2: 'please wait until full content is loaded',
-        });
-      }
+  //     if (viewedItems.filter(item => !item.isLoaded).length !== 0) {
+  //       return Toast.show({
+  //         type: 'info',
+  //         text1: label,
+  //         text2: 'please wait until full content is loaded',
+  //       });
+  //     }
 
-      if (initial) {
-        const next = initial.next();
-        if (next && !next.done) {
-          setTimeout(() => {
-            setItems(next.value);
-          }, 100);
-        } else {
-          setItems([]);
-        }
-        return;
-      }
+  //     if (initial) {
+  //       const next = initial.next();
+  //       if (next && !next.done) {
+  //         setTimeout(() => {
+  //           setItems(next.value);
+  //         }, 100);
+  //       } else {
+  //         setItems([]);
+  //       }
+  //       return;
+  //     }
 
-      const next = iterator!.next();
-      if (next && !next.done) {
-        setTimeout(() => {
-          setItems(next.value);
-        }, 100);
-      } else {
-        setItems([]);
-      }
-    },
-    [items, data, iterator],
-  );
+  //     const next = iterator!.next();
+  //     if (next && !next.done) {
+  //       setTimeout(() => {
+  //         setItems(next.value);
+  //       }, 100);
+  //     } else {
+  //       setItems([]);
+  //     }
+  //   },
+  //   [items, data, iterator],
+  // );
 
-  useEffect(() => {
-    setItems([]);
-    const iterator = (function* () {
-      const dataCopy: typeof data = [];
-      let prevPos = 0;
+  // useEffect(() => {
+  //   setItems([]);
+  //   const iterator = (function* () {
+  //     const dataCopy: typeof data = [];
+  //     let prevPos = 0;
 
-      while (dataCopy.length !== data.length) {
-        dataCopy.push(...data.slice(prevPos, prevPos + 5));
-        prevPos += 5;
-        yield dataCopy;
-      }
-    })();
+  //     while (dataCopy.length !== data.length) {
+  //       dataCopy.push(...data.slice(prevPos, prevPos + 5));
+  //       prevPos += 5;
+  //       yield dataCopy;
+  //     }
+  //   })();
 
-    setIterator(iterator);
-    nextSet(iterator);
-  }, [data]);
+  //   setIterator(iterator);
+  //   nextSet(iterator);
+  // }, [data]);
 
   return (
     <View style={styles.container}>
@@ -367,9 +368,9 @@ export default function FilesList({
             }}>
             {label}
           </Text>
-          {viewedItems.filter(item => !item.isLoaded).length !== 0 && (
+          {/* {viewedItems.filter(item => !item.isLoaded).length !== 0 && (
             <Circle size={16} indeterminate={true} />
-          )}
+          )} */}
         </View>
         <View
           style={{
@@ -395,7 +396,7 @@ export default function FilesList({
           <NoDataFound />
         ) : (
           <FlatList
-            data={items}
+            data={data}
             renderItem={renderFile}
             keyExtractor={item => item.id}
             horizontal
@@ -403,10 +404,9 @@ export default function FilesList({
             initialNumToRender={5}
             removeClippedSubviews={true}
             maxToRenderPerBatch={5}
-            onEndReached={() => nextSet()}
-            viewabilityConfigCallbackPairs={
-              viewabilityConfigCallbackPairs.current as unknown as ViewabilityConfigCallbackPair[]
-            }
+            // viewabilityConfigCallbackPairs={
+            //   viewabilityConfigCallbackPairs.current as unknown as ViewabilityConfigCallbackPair[]
+            // }
             viewabilityConfig={{
               minimumViewTime: 200,
             }}
