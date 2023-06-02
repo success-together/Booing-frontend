6f03ac5b1f6b69be20c33425e173f7a119514846
oleksandr-harvart <oharvart7@gmail.com>
2023-06-02 10:32:47 +0300

    bug fix 11

--- src/screens/Authentication/Register/Register.tsx
+++ src/screens/Authentication/Register/Register.tsx
@@ -470,7 +470,8 @@ function Register({navigation}: {navigation: any}) {
               height: 60,
             }}
             onPress={onSubmit}>
-            <Text style={[styles.text, {color: '#fff'}]}>Sign Up</Text>
+            
+            <Text style={[styles.text, {color: '#fff'}]}>SIGN UP</Text>
           </Pressable>
         </LinearGradient>
       </View>
